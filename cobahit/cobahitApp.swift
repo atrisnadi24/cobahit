2189fbeae506acdfcc273ee770e3b93ca8fb2ecf
Alfonsius Trisnadi <atrisnadi24@ilb.idserve.net>
2024-05-06 16:32:33 +0700

    Initial Commit

---
+++ cobahit/cobahitApp.swift
@@ -0,0 +1,17 @@
+//
+//  cobahitApp.swift
+//  cobahit
+//
+//  Created by Alfonsius  Trisnadi on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct cobahitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
