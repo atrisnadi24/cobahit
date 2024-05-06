9d09b66ef46952addf508d1fbe90a689cf7ec325
Alfonsius Trisnadi <atrisnadi24@ilb.idserve.net>
2024-05-06 16:46:40 +0700

    fisrt commit

--- cobahit/ContentView.swift
+++ cobahit/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("tambah text")
+            Text("Tambah lagi")
         }
         .padding()
     }
---
+++ cobahit/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  cobahit
+//
+//  Created by Alfonsius  Trisnadi on 06/05/24.
+//
+
+import Foundation
